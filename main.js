493f80ab0d8ba5177d95fba38790c90c8392cf4f
Sabarna22 <84438184+Sabarna22@users.noreply.github.com>
2021-09-15 20:37:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function preload() {
+  
+ noseX = 0;
+ noseY = 0;
+ GameStatus = "";
+ 
+ 
+ function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
@@ -9,14 +15,45 @@ function setup() {
 	canvas.parent('canvas');
 
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotposes);
+
+}
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+  
+  function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	  console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +", noseY = " + noseY);
+  }
 }
 
 function draw() {
 	game()
 }
 
+function game() 
+{
+	 console.log("noseX = " + noseX +" ,noseY = "+ noseY);
+	instializeInDraw();
+	moveEnviroment(mario);
+	drawSprites();
+}
 
-
-
-
+function startGame()
+{
+	GameStatus = "start";
+	document.getElementById("status").innerHTML = "Game Is Loading";
+}
 
